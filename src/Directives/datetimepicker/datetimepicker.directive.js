e939210530961aaac5556febf574508dfc632663
jeonghwan <ej88ej@gmail.com>
2015-07-18 23:16:05 +0900

    Directive: datetimepicker (enhance validate date format)

--- src/Directives/datetimepicker/datetimepicker.directive.js
+++ src/Directives/datetimepicker/datetimepicker.directive.js
@@ -13,14 +13,10 @@ angular.module('chris.util')
         restrict: 'A',
         link: function (scope, element, attrs, ngModelCtrl) {
 
+          if (!ngModelCtrl) return;
+
           function updateModelValue () {
             ngModelCtrl.$setViewValue(element[0].value)
-          }
-
-          function validateDateFormat (val) {
-            var valid = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}$/.test(val);
-            ngModelCtrl.$setValidity('dateFormat', valid);
-            return valid ? val: undefined;
           }
 
           $(element).datetimepicker({
@@ -31,8 +27,10 @@ angular.module('chris.util')
             onSelectTime: updateModelValue
           });
 
-          ngModelCtrl.$parsers.push(validateDateFormat); // input 요소의 값이 변경될 경우 호출할 함수 목록
-          ngModelCtrl.$formatters.push(validateDateFormat); // 모델 값이 변경되면 호출되는 함수 목록
+          ngModelCtrl.$validators.dateFormat = function (modelValue, viewValue) {
+            return modelValue === '' ||
+                   /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}$/.test(modelValue);
+          }
         }
       };
     });
